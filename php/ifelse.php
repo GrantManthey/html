ce51209b4d3428c732129b35ed5aabcef8ff6114
GrantManthey <grant.manthey8580@gmail.com>
2016-10-25 12:25:10 -0700

    Add files via upload

---
+++ php/ifelse.php
@@ -0,0 +1,118 @@
+ 
+<!DOCTYPE html>
+<html><head><title>if else</title>
+
+<style>
+.r1 { background-color:#3F31CE;
+		color:rgb(190,150,217);
+	}
+
+.r2 { background-color:#EEEEEF5;
+		color: rgb(190,150,217);
+	}
+</style>
+</head>
+<body>
+	<center>
+<h1>
+if else php example
+</h1>
+
+
+<h1><h1><div class="r1" >* * * * * * * * Once upon a time there was a lovely princess. But she had an enchantment * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * upon her of a fearful sort which could only be broken by love's first kiss. * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * She was locked away in a castle guarded by a terrible fire-breathing dragon.  * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * Many brave knights had attempted to free her from this dreadful prison,  * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * but non prevailed. She waited in the dragon's keep in the highest room of  * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * the tallest tower for her true love and true love's first kiss. (laughs)   * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * Like that's ever gonna happen. What a load of - (toilet flush) * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * Allstar - by Smashmouth begins to play. Shrek goes about his day. * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * While in a nearby town, the villagers get together to go after the ogre. * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * NIGHT - NEAR SHREK'S HOME * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * *  MAN1 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * *  Think it's in there? * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * MAN2 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * All right. Let's get it! * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * *  MAN1 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * Whoa. Hold on. Do you know what that thing can do to you? * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * MAN3 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * Yeah, it'll grind your bones for it's bread. * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * Shrek sneaks up behind them and laughs. * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * SHREK * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * Yes, well, actually, that would be a giant. Now, ogres, oh they're much worse. They'll make a suit from your freshly peeled skin. * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 21 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 22 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 23 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 24 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 25 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 26 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 27 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 28 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 29 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 30 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 31 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 32 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 33 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 34 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 35 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 36 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 37 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 38 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 39 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 40 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 41 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 42 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 43 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 44 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 45 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 46 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 47 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 48 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 49 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 50 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 51 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 52 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 53 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 54 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 55 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 56 * * * * * * * *<br /></div><div
+class="r2" >* * * * * * * * 57 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 58 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 59 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 60 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 61 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 62 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 63 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 64 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 65 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 66 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 67 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 68 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 69 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 70 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 71 * * * * * * * *<br /></div><div
+class="r1" >* * * * * * * * 74 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 75 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 76 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 77 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 78 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 79 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 80 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 81 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 82 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 83 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 84 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 85 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 86 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 87 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 88 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 89 * * * * * * * *<br /></div><div 
+class="r1" >* * * * * * * * 90 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 91 * * * * * * * *<br /></div><div
+class="r1" >* * * * * * * * 94 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 95 * * * * * * * *<br /></div><div
+class="r1" >* * * * * * * * 98 * * * * * * * *<br /></div><div 
+class="r2" >* * * * * * * * 99 * * * * * * * *<br /></div></h1>
+</center>
+</body>
+</html>
